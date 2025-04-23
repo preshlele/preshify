59ad2467471bb5bed3cc2ee836257789eaa13733
Emmanuel Mensah <emomensa@gmail.com>
2025-04-23 11:22:15 +0000

    chore: folder structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css:['src/assets/styles/main.css'],
+  css:['app/assets/styles/main.css'],
   vite:{
     plugins: [
       tailwindcss(),
